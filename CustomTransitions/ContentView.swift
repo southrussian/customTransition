8d685fd2148886aa391f6c3612a3ddb638e3790f
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-02-25 17:01:24 +0300

    Initial Commit

---
+++ CustomTransitions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTransitions
+//
+//  Created by Danil Peregorodiev on 25.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomTransitions/CustomTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTransitionsApp.swift
+//  CustomTransitions
+//
+//  Created by Danil Peregorodiev on 25.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
